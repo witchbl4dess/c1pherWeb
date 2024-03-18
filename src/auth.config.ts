1548687910189a4556684649769d295287a2d382
witchbl4des <menzo.smit-adam@epitech.eu>
2024-03-18 05:00:15 +0100

    github passport

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,6 @@
 import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 import { getUserByEmail } from "@/data/user";
 import { LoginSchema } from "@/schema";
@@ -6,6 +8,14 @@ import bcrypt from "bcryptjs";
 
 export default {
     providers: [
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+        GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        }),
         Credentials({
             async authorize(credentials) {
                 const validatedFields = LoginSchema.safeParse(credentials);
