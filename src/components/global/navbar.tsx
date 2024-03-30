3d0983f01b80d12d5bc15ce5a596784c47c724a5
witchbl4des <menzo.smit-adam@epitech.eu>
2024-03-30 19:34:26 +0100

    Add navigation links to the navbar

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import icon from "@/assets/iconc1.png"
+import Link from "next/link";
 
 type Props = {}
 
@@ -8,10 +9,16 @@ const Navbar = async (props: Props) => {
     return (
         <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
             <aside className="flex items-center gap-[2px]">
-                <p className="text-3xl font-bold">C1</p>
-                <Image src={icon} width={15} height={15} alt="c1pherlogo" className="shadow-sm"/>
-                <p className="text-3xl font-bold">pher</p>
+                <p className="text-3xl font-bold">c1pher.</p>
             </aside>
+            <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+                <ul className="flex items-center gap-4 list-none">
+                    <li><Link href="#">Presention</Link></li>
+                    <li><Link href="#">Download</Link></li>
+                    <li><Link href="#">Documentation</Link></li>
+                    <li><Link href="#">About</Link></li>
+                </ul>
+            </nav>
         </header>
     )
 }
