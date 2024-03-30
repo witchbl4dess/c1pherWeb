45618dba67073fddb28ab1bf217831d66e090272
witchbl4des <menzo.smit-adam@epitech.eu>
2024-03-30 20:25:15 +0100

    Add tsparticles library and LampComponents section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
               <div className="flex items-center flex-col">
                 <Button size={'lg'} className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500">
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                    Start For Free Today
+                    Join us 🔧
                   </span>
                 </Button>
                 <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
@@ -27,6 +27,9 @@ export default function HomePage() {
             }
           ></ContainerScroll>
           </div>
+      </section>
+      <section className="mt-[-500px]">
+        <LampComponents />
       </section>
     </main>
   )
