d6b3ea6f1762ffa043b46151d916f5d14915a5bd
witchbl4des <menzo.smit-adam@epitech.eu>
2024-03-30 16:42:07 +0100

    fix token auto destroy (15min)

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -12,7 +12,7 @@ import * as z from "zod";
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
   const existingToken = await getTwoFactorTokenByMail(email);
 
   if (existingToken) {
