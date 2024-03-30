cec784c3323ed0ea0c7fbb5b3b8ed465236a4e8f
witchbl4des <menzo.smit-adam@epitech.eu>
2024-03-30 21:41:35 +0100

    Add new UI components

--- src/components/global/connect-parallax.tsx
+++ src/components/global/connect-parallax.tsx
@@ -105,7 +105,7 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+        The Ultimate <br /> recovery windows ISO
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
         We build beautiful products with the latest technologies and frameworks.
