3efe947eb5b21ea9dc66c721701b5a90e9495f95
witchbl4des <menzo.smit-adam@epitech.eu>
2024-03-31 03:37:02 +0200

    Refactor user.ts and index.ts

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,12 +1,13 @@
 import { db } from "@/lib/db";
 
 export const getUserByEmail = async (email: string) => {
-    try {
-        const user = await db.user.findUnique({ where: { email } })
-        return user;
-    } catch {
-        return null;
-    }
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+
+    return user;
+  } catch {
+    return null;
+  }
 };
 
 export const getUserById = async (id: string) => {
